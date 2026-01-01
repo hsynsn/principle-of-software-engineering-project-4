e8cd426a3104ac3b26ed22c9723ec06eaad339ef
OguzBartuKilic <22COMP1024@isik.edu.tr>
2026-01-01 18:42:47 +0300

    satisfying change two

--- hwk4/JavaProgram.java
+++ hwk4/JavaProgram.java
@@ -2,7 +2,14 @@ class JavaProgram {
 
     static void function(int n1, int n2){
         for(int i = n1 + 1; i<n2; i++){
-            System.out.println(i);
+            if(i%2 == 1){
+                System.out.println(i);
+            }
         }
     }
+
+    public static void main(String[] args) {
+        function(1,10);
+    }
 }
+
