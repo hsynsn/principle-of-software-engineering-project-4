f9edf024c466998403b82c28c0f4b8d1670987d2
OguzBartuKilic <22COMP1024@isik.edu.tr>
2026-01-01 19:13:05 +0300

    move JavaProgram into hwk4

--- hwk4/JavaProgram.java
+++ hwk4/JavaProgram.java
@@ -2,7 +2,11 @@ class JavaProgram {
 
     static void function(int n1, int n2){
         for(int i = n1 + 1; i<n2; i++){
-            System.out.println(i);
+            if(i%2 == 0){
+                System.out.println(i);
+            }
         }
     }
+
 }
+
